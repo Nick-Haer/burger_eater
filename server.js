437fd1b571ad6b6a0ff8b5c20e516c5bf2d7927d
Nick.Haer <nhaer123@gmail.com>
2019-09-18 16:47:42 -0500

    Added server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require(`express`);
+
+const app = express()
+
+const router = require(``)
+
+const PORT = process.env.PORT || 3000;
+
+
+
+
+app.use(`/`, )
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`)
+})
