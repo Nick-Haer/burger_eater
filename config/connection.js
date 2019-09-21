fd51aa88a2c8f845cf9b93a9d1a7db5262ae1db0
Nick.Haer <nhaer123@gmail.com>
2019-09-21 12:16:26 -0500

    update heroku information

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 const mysql = require(`mysql`);
 
 const connection = mysql.createConnection({
-    host: "localhost",
+    host: "s9xpbd61ok2i7drv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "63_TRooTandTree!",
-    database: "burgers_db"
+    user: "bwknhqpg2bjnosj7",
+    password: "g6g2kcfpc6w6hzr3",
+    database: "vfehq9vxo8uabrsz"
 })
 
 connection.connect((err) => {
